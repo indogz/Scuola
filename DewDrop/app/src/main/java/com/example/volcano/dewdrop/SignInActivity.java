08bbe0351fcbabc817b9c542816f9cb48e816b23
Gandalf <me.me@myself.com>
2017-03-26 14:04:18 +0200

    Maturità

---
+++ DewDrop/app/src/main/java/com/example/volcano/dewdrop/SignInActivity.java
@@ -0,0 +1,14 @@
+package com.example.volcano.dewdrop;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignInActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
